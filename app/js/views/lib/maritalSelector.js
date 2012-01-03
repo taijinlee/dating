a7b521b3a46424b39c57243fc96fe68fcddad913
Tai-Jin Lee <taijin@gmail.com>
2012-01-02 23:15:28 -0800

    fixing marital selector size

--- app/js/views/lib/maritalSelector.js
+++ app/js/views/lib/maritalSelector.js
@@ -8,7 +8,7 @@ define([
 
     initialize: function(options) {
 
-      $(this.el).attr('name', 'marital_status');
+      $(this.el).attr({'name': 'marital_status', 'class': 'small' });
 
       var maritalOptions = {
         '': 'Marital Status:',
@@ -19,7 +19,7 @@ define([
       };
 
       for (var name in maritalOptions) {
-        var attr = { value: name }
+        var attr = { 'value': name }
         if (name == options.marital_status) {
           attr['selected'] = 'selected';
         }
